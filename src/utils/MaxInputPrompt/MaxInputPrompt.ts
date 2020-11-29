e1ecff7ca4b977486e45e483ebec4637d1ac3736
Jimmy Guzman <jimmy.guzman@outlook.com>
2020-11-29 17:35:08 -0600

    refactor(commit): ♻️ reduced `@ts-ignore` in `MaxInputPrompt`

--- src/utils/MaxInputPrompt/MaxInputPrompt.ts
+++ src/utils/MaxInputPrompt/MaxInputPrompt.ts
@@ -1,31 +1,36 @@
-/* eslint-disable lines-around-comment */
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import chalk from 'chalk'
+import { Answers, Question } from 'inquirer'
 import InputPrompt from 'inquirer/lib/prompts/input'
+import { Interface as ReadLineInterface } from 'readline'
 
-// TODO: eliminate @ts-ignore
-// TODO: MaxInputPrompt ==> LimitedInputPrompt to account for min chars
-export class MaxInputPrompt extends InputPrompt {
+interface MaxInputQuestion extends Question {
+  maxLength: number
+  leadingLabel: string | ((answers: Answers) => void)
+}
+
+type MaxInputPromptType = new (
+  question: MaxInputQuestion,
+  readLine: ReadLineInterface,
+  answers: Answers
+) => InputPrompt<MaxInputQuestion>
+
+export class MaxInputPrompt extends InputPrompt<MaxInputQuestion> {
   private _leadingLabel: string
   private _leadingLength: number
   private _maxLength: number
-  constructor(...args: ConstructorParameters<typeof InputPrompt>) {
+  constructor(...args: ConstructorParameters<MaxInputPromptType>) {
     super(...args)
-    // @ts-ignore
+
     if (!this.opt.maxLength) {
       this.throwParamError('maxLength')
     }
-    // @ts-ignore
+
     this._maxLength = this.opt.maxLength
 
-    // @ts-ignore
     if (this.opt.leadingLabel) {
-      // @ts-ignore
       if (typeof this.opt.leadingLabel === 'function') {
-        // @ts-ignore
         this._leadingLabel = `${this.opt.leadingLabel(this.answers)}`
       } else {
-        // @ts-ignore
         this._leadingLabel = `${this.opt.leadingLabel}`
       }
     } else {
@@ -41,11 +46,13 @@ export class MaxInputPrompt extends InputPrompt {
 
   onKeypress(): void {
     if (this.rl.line.length > this._maxLength - this._leadingLength) {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       this.rl.line = this.rl.line.slice(
         0,
         this._maxLength - this._leadingLength
       )
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
       this.rl.cursor--
     }
@@ -68,10 +75,11 @@ export class MaxInputPrompt extends InputPrompt {
   }
 
   render(error?: string): void {
-    const isAnswered: boolean = this.status === 'answered'
-    const bottomContent: string = error ? chalk.red('>> ') + error : ''
-    const message = `${this.getQuestion()}${this.getCharsLeftText()}
-  ${this._leadingLabel} ${this.rl.line}`
+    const isAnswered = this.status === 'answered'
+    const bottomContent = error ? chalk.red('>> ') + error : ''
+    const message = `${this.getQuestion()}${this.getCharsLeftText()}\n${
+      this._leadingLabel
+    } ${this.rl.line}`
 
     this.screen.render(
       isAnswered ? `${this.getQuestion()}${chalk.cyan(this.answer)}` : message,
